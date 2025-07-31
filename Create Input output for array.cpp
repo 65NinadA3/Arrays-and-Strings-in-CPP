b894adc108ef0d7a4b0ac1b1245d9297b61869f8
65NinadA3 <ninadphatak123@gmail.com>
2025-07-31 11:44:32 +0530

    Create Create Input output for array.cpp

---
+++ Create Input output for array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int numbers[7];  
+    cout << "Enter 7 numbers:" << endl;
+    for (int i = 0; i < 7; i++) {
+        cin >> numbers[i];
+    }
+    cout << "You entered:" << endl;
+    for (int i = 0; i < 7; i++) {
+        cout << numbers[i] << endl;
+    }
+    return 0;
+}
