7d95b0cd0ef0c9f5f17af1112acc221f8bf217b8
65NinadA3 <ninadphatak123@gmail.com>
2025-07-31 11:45:20 +0530

    Create Search Element in an array.cpp

---
+++ Search Element in an array.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+    int marks[5] = {10, 25, 7, 42, 15};
+    int key;
+    bool found = false;
+    cout << "Enter the number you want to search for: ";
+    cin >> key; 
+    for (int i = 0; i < 6; i++) {
+       if (key == marks[i]) {
+            cout << "Key found at location: " << i+1 << endl;  
+            found = true; 
+            break;
+        }
+    }
+    if(!found){
+        cout << "Key not found in the array." << endl; 
+    }
+    return 0; 
+}
