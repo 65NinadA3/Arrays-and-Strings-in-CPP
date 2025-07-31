d33bcadc28055f2cd512f2bce326ae7a89bcb54a
65NinadA3 <ninadphatak123@gmail.com>
2025-07-31 11:51:01 +0530

    Create Taking string as input and display it.cpp

---
+++ Taking string as input and display it.cpp
@@ -0,0 +1,12 @@
+//Name : Ninad Phatak
+//PRN : 24070123065
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+int main() {
+    char str[50];
+    cout << "Enter a string: ";
+    cin >> str;  
+    cout <<"You entered:"<< str << endl;
+    return 0;
+}
